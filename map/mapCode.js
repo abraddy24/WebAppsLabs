e59c884a6ea475ea1abf6ca41485885d31621d28
Alex Braddy <braddya16@hanover.edu>
2015-01-25 18:59:14 -0500

    tests map to see if makeMap returns object and is defined, full map implementation (tentative). Close #13

--- map/mapCode.js
+++ map/mapCode.js
@@ -7,9 +7,49 @@
 var makeMap = function() {
 // All your code will go inside this function
    // This object should contain the methods you want to expose:
-   var o;
+   var o = {
+      has:  function(key){
+         if(key in o){
+            return true;
+         }
+         return false;
+      },
+
+      lookup: function(key){
+         if(o.has(key)===false){
+            throw new Error('Key does not exist');
+         }
+         return o.key;
+      },
+
+      add: function(key,value){
+         if(o.has(key)===true)
+         {
+            throw new Error('Attempt to add key that already exists');
+         }
+         o.key = value;
+         storedPairs.key = value;
+         return o;
+      },
+
+      update: function(key,value){
+         if(o.has(key)===false){
+            throw new Error('Key does not exist');
+         }
+         o.key = value;
+         return o;
+      },
+
+      remove: function(key){
+         if(o.has(key)===false){
+            throw new Error('Key does not exist');
+         }
+         delete(o.key);
+         delete(storedPairs.key);
+      }
+   };
    // Use this object to store the key-value pairs:
-   var storedPairs;
+   var storedPairs = {};
 
    // Add initialization code here
 
