57c185044f35c3db1f530f5f577d879d5af39669
Alex Braddy <braddya16@hanover.edu>
2015-02-01 15:12:36 -0500

    makeTaskFromObject testing completed. Close #36

--- task.spec.js
+++ task.spec.js
@@ -9,6 +9,18 @@ expect = require('./chai.js').expect;
 
 Task = require('./task.js');
 
+
+function randomStr()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
 // ADD YOUR TESTS HERE
 describe('Your makeNewTask function', function() {
 	var t;
@@ -21,4 +33,33 @@ describe('Your makeNewTask function', function() {
 	});
 });
 
+
+describe('Your makeTaskFromObject function', function(){
+	var obj, task, s, tags, tag1, tag2;
+	tag1 = randomStr();
+	tag2 = randomStr();
+	s = randomStr();
+	tags = [tag1, tag2];
+	obj = {'title': s, 'tags': tags};
+	task = Task.fromObject(obj);
+	console.log(task);
+
+	it('returns an object', function() {
+		expect(task).to.be.a('object');
+	});
+
+	it('returns an object with keys', function() {
+		expect(task).to.have.ownProperty('title', 'completedTime', 'tags', 'id');
+	});
+
+	it('returns object with correct title', function() {
+		expect(task.title).to.equal(s);
+	});
+
+	it('returns obect with correct tags', function() {
+		expect(task.tags).to.have.length(2);
+		expect(task.tags).to.include(tag1);
+		expect(task.tags).to.include(tag2);
+	});
+});
 
