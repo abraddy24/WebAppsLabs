aad754e4589e6af5674511ba5108749586a6d410
Alex Braddy <braddya16@hanover.edu>
2015-02-01 15:01:19 -0500

    testing for makeNewTask() is completed and passing. Close #35

--- task.spec.js
+++ task.spec.js
@@ -10,3 +10,15 @@ expect = require('./chai.js').expect;
 Task = require('./task.js');
 
 // ADD YOUR TESTS HERE
+describe('Your makeNewTask function', function() {
+	var t;
+	t = Task.new();
+	it('returns an object', function() {
+		expect(t).to.be.a('object');
+	});
+	it('returns an object with keys', function() {
+		expect(t).to.have.ownProperty('title','completedTime', 'tags', 'id');
+	});
+});
+
+
