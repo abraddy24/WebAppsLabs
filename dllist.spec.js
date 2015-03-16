8211fb170ed989575c3ce8448980c7f224d52a6e
Alex Braddy <braddya16@hanover.edu>
2015-03-16 18:00:14 -0400

    edited tests to be more extensive.

--- dllist.spec.js
+++ dllist.spec.js
@@ -52,23 +52,29 @@ describe("Your last, insertAt, unshift, and push functions", function(){
       list.push(0);
       list.push(1);
       expect(list.last().value).to.equal(1);
+      list.push(8);
+      list.push(12);
+      list.push(5);
+      expect(list.last().value).to.equal(5);
   });
   it("insertAt correctly inserts items", function(){
       list.push(0);
       list.insertAt(3, list.first());
       expect(list.last().value).to.equal(3);
+      list.push(6);
+      list.insertAt(9, list.last());
+      expect(list.last().value).to.equal(9);
   });
   it("unshift adds a new element at the beginning of the list", function(){
       list.push(2);
       list.push(1);
       list.unshift(0);
       expect(list.first().value).to.equal(0);
-  });
-  it("push adds a new element at the end of the list", function(){
-      list.push(1);
-      expect(list.last().value).to.equal(1);
-      list.push(2);
-      expect(list.last().value).to.equal(2);
+      list.push(8);
+      list.push(6);
+      list.unshift(9);
+      list.unshift(10);
+      expect(list.first().value).to.equal(10);
   });
 });
 describe("Your endAt, remove, pop, and shift function", function(){
@@ -87,6 +93,12 @@ describe("Your endAt, remove, pop, and shift function", function(){
       list.push(4);
       list.push(5);
       expect(list.endAt(list.sentinel.next.next.next).length()).to.equal(3);
+      list.push(7);
+      list.push(9);
+      list.push(10);
+      list.push(15);
+      list.push(20);
+      expect(list.endAt(list.sentinel.next.next.next.next.next).length()).to.equal(5);
   });
   it("remove removes an item", function(){
       list.push(1);
@@ -98,18 +110,22 @@ describe("Your endAt, remove, pop, and shift function", function(){
       expect(list.sentinel.next.value).to.equal(6);
       expect(list.sentinel.prev.value).to.equal(6);
   });
-  it("pop removes the last item and throws error if empty", function(){
-     // expect(list.pop()).to.throw(String);
+  it("pop removes the last item", function(){
       list.push(2);
-      list.push(3);
-      expect(list.pop()).to.equal(3);
+      list.push(8);
+      expect(list.pop()).to.equal(8);
   });
-  it("shift removes first item and throws error if empty", function(){
-      // expect(list.shift()).to.throw(Error);
+  it("pop throws error if empty list", function(){
+    expect(function() { list.pop(); }).to.throw(Error);
+  });
+  it("shift removes first item", function(){
       list.push(8);
       list.push(9);
       expect(list.shift()).to.equal(8);
   });
+  it("shift throws error if empty", function(){
+    expect(function() { list.shift(); }).to.throw(Error);
+  })
 });
 describe("Your isFist, isLast, iterator, and for each fucntions", function(){
   var list, item, item2, value;
@@ -123,31 +139,34 @@ describe("Your isFist, isLast, iterator, and for each fucntions", function(){
       list.push(9);
       expect(list.isFirst(list.sentinel.next)).to.equal(true);
       expect(list.isFirst(list.sentinel.prev)).to.equal(false);
+      list.unshift(8);
+      expect(list.isFirst(list.sentinel.prev)).to.equal(true);
+      expect(list.isFirst(list.sentinel.next)).to.equal(false);
   });
   it("isLast is true when argument is the last item", function(){
-      list.push(5);
-      list.push(6);
+      list.push(8);
+      list.push(1);
       expect(list.isLast(list.sentinel.next)).to.equal(false);
       expect(list.isLast(list.sentinel.prev)).to.equal(true);
   });
   it("iterator returns an iterator for the dllist", function(){
-      var it = list.iterator();
+      var itr = list.iterator();
       expect(it.hasNext()).to.equal(false);
       list.push(1);
       list.push(2);
-      it = list.iterator();
-      expect(it.hasNext()).to.equal(true);
-      expect(it.next()).to.equal(1);
-      expect(it.next()).to.equal(2);
-      expect(it.hasNext()).to.equal(false);
+      itr = list.iterator();
+      expect(itr.hasNext()).to.equal(true);
+      expect(itr.next()).to.equal(1);
+      expect(itr.next()).to.equal(2);
+      expect(itr.hasNext()).to.equal(false);
   });
   it("forEach applies a function to each value in the list", function(){
-      var fun = function(val){
-        console.log(2 * val);
+      var ehh = function(v){
+        console.log(v);
       };
       list.push(15);
       list.push(16);
-      list.forEach(fun);
+      list.forEach(ehh);
   });
 });
 describe("Your toArray, itrateFrom, and reverseIterateFrom functions", function(){
@@ -158,31 +177,33 @@ describe("Your toArray, itrateFrom, and reverseIterateFrom functions", function(
   value += 1;
   });
   it("toArray returns an array of the list's values", function(){
-      list.push(14);
-      list.push(15);
-      expect(list.toArray()[ 0 ]).to.equal(14);
-      expect(list.toArray()[ 1 ]).to.equal(15);
+      list.push(5);
+      list.push(9);
+      list.push(5);
+      list.ushift(7);
+      expect(list.toArray()[ 0 ]).to.equal(7);
+      expect(list.toArray()[ 1 ]).to.equal(5);
   });
   it("iterateFrom returns an iterator starting from a specific item", function(){
-      var it;
-      list.push(10);
-      list.push(11);
-      list.push(12);
-      it = list.iterateFrom(list.first());
-      expect(it.next()).to.equal(10);
-      expect(it.next()).to.equal(11);
-      expect(it.next()).to.equal(12);
+      var itr;
+      list.push(18);
+      list.push(14);
+      list.push(16);
+      itr = list.iterateFrom(list.first());
+      expect(itr.next()).to.equal(18);
+      expect(itr.next()).to.equal(14);
+      expect(itr.next()).to.equal(16);
   });
   it("reverseIterateFrom  returns an iterator that starts at the end", function(){
-      var it;
-      list.push(15);
-      list.push(14);
-      list.push(13);
-      list.push(12);
-      it = list.reverseIterateFrom(list.last());
-      expect(it.next()).to.equal(12);
-      expect(it.next()).to.equal(13);
-      expect(it.next()).to.equal(14);
-      expect(it.next()).to.equal(15);
+      var itr;
+      list.push(8);
+      list.push(9);
+      list.push(11);
+      list.push(16);
+      itr = list.reverseIterateFrom(list.last());
+      expect(itr.next()).to.equal(16);
+      expect(itr.next()).to.equal(11);
+      expect(itr.next()).to.equal(9);
+      expect(itr.next()).to.equal(8);
   });
 });
