d8cde1056c1c06d28d387ac927fc0a247aa805ac
Alex Braddy <braddya16@hanover.edu>
2015-03-15 20:44:28 -0400

    linted file

--- dllist.js
+++ dllist.js
@@ -1,176 +1,165 @@
 /*
- * dllist.js
- *
- * Contains implementation for a double-linked list "class"
- */
-
+* dllist.js
+*
+* Contains implementation for a double-linked list "class"
+*/
 var Iterator, DLList, proto;
-
 Iterator = require("./iterator");
-
 /*
- *       Constructors
- */
-
+* Constructors
+*/
 function makeNewList() {
-   var lst, sentinel;
-
-   lst = Object.create(proto);
-   sentinel = { value: null};
-   sentinel.next = sentinel;
-   sentinel.prev = sentinel;
-   lst.sentinel = sentinel;
-   return lst;
+	var lst, sentinel;
+	lst = Object.create(proto);
+	sentinel = { value: null };
+	sentinel.next = sentinel;
+	sentinel.prev = sentinel;
+	lst.sentinel = sentinel;
+	return lst;
 }
-
-
 /*
- *       Prototype / Instance methods
- */
-
+* Prototype / Instance methods
+*/
 proto = {
-   // Add instance methods here
-   isEmpty: function isEmpty(){
-   		if(this.sentinel.next === this.sentinel && this.sentinel.prev === this.sentinel && this.sentinel.value === null){
-   			return true;
-   		}
-   		return false;
-   	},
-   	length: function length(){
-   		var len; temp;
-   		len = 0;
-   		temp = this.sentinel;
-   		while(temp.next !== this.sentinel){
-   			len += 1;
-   			temp = temp.next;
-   		}
-   		return len;
-   	},
-   	first: function first(){
-   		if(this.isEmpty()){
-   			throw "Cannot check on empty list";
-   		}
-   		return this.sentinel.next;
-   	},
-   	last: function last(){
-   		if(this.isEmpty()){
-   			throw "Cannot check on empty list";
-   		}
-   		return this.sentinel.prev;
-   	},
-   	insertAt: function insertAt(val, node){
-   		var tmp = {value: val, next: null, prev: null};
-   		tmp.next = node.next;
-   		tmp.prev = node;
-   		node.next.prev = tmp;
-   		node.next = tmp;
-   		return tmp;
-   	},
-   	unshift: function unshift(val){
-   		this.insertAt(val, this.sentinel);
-   		return val;
-   	},
-   	push: function push(val){
-   		this.insertAt(val, this.sentinel.prev);
-   		return val;
-   	},
-   	endAt: function endAt(item){
-   		var tmp = this.sentinel;
-   		if(tmp === tmp){
-   			tmp.next = this.sentinel;
-   			this.sentinel.prev = tmp;
-   		}
-   		while(tmp !== item){
-   			tmp = tmp.next;
-   		}
-   		return this;
-   	},
-   	remove: function remove(item){
-   		var tmp = this.first();
-   		if(tmp.next === item){
-   			tmp.next = item.next;
-   			item.next.prev = tmp;
-   		}
-   		while(tmp.next !== item){
-   			tmp = tmp.next;
-   		}
-   		return item.value;
-   	},
-   	pop: function pop(){
-   		return this.reomve(this.last);
-   	},
-   	shift: function shift(){
-   		return this.remove(this.first());
-   	},
-   	isFirst: function isFirst(item){
-   		if(this.first() === item){
-   			return true;
-   		}
-   		return false;
-   	},
-   	isLast: function isLast(item){
-   		if(this.last() === item){
-   			return true;
-   		}
-   		return false;
-   	},
-   	iterator: function iterator(){
-   		var itr, that;
-   		itr = this.sentinel;
-   		that = this;
-   		return Iterator.new(
-   			function next(){
-   				itr = itr.next;
-   				return itr.value;
-   			},
-   			function hasNext(){
-   				return itr.next !== that.sentinel;
-   			});
-   	},
-   	forEach: function forEach(f){
-   		return this.iterator().forEach(f);
-   	},
-   	toArray: function toArray(){
-   		return this.iterator().toArray();
-   	},
-   	iterateFrom: function iterateFrom(item){
-   		var that = this;
-   		item = item.prev;
-   		return Iteratior.new(
-   			function next(){
-   				item = item.next;
-   				return item.value;
-   			},
-   			function hasNext(){
-   				return item.next !== that.sentinel;
-   			}
-   		);
-   	},
-   	reverseIterateFrom: function reverseiterateFrom(item){
-   		var that = this;
-   		item = item.next;
-   		return Iteratior.new(
-   			function next(){
-   				item = item.prev;
-   				return item.value;
-   			},
-   			function hasNext(){
-   				return item.prev !== that.sentinel;
-   			}
-   		);
-   	}
+	// Add instance methods here
+	isEmpty: function isEmpty(){
+	if (this.sentinel.next === this.sentinel && this.sentinel.prev === this.sentinel
+			&& this.sentinel.value === null){
+		return true;
+	}
+	return false;
+},
+length: function length(){
+	var len, temp;
+	len = 0;
+	temp = this.sentinel;
+	while (temp.next !== this.sentinel){
+		len += 1;
+		temp = temp.next;
+	}
+	return len;
+},
+first: function first(){
+	if (this.isEmpty()){
+		throw "Cannot check on empty list";
+	}
+	return this.sentinel.next;
+},
+last: function last(){
+	if (this.isEmpty()){
+		throw "Cannot check on empty list";
+	}
+	return this.sentinel.prev;
+},
+insertAt: function insertAt(val, node){
+	var tmp = { value: val, next: null, prev: null };
+	tmp.next = node.next;
+	tmp.prev = node;
+	node.next.prev = tmp;
+	node.next = tmp;
+	return tmp;
+},
+unshift: function unshift(val){
+	this.insertAt(val, this.sentinel);
+	return val;
+},
+push: function push(val){
+	this.insertAt(val, this.sentinel.prev);
+	return val;
+},
+endAt: function endAt(item){
+	var tmp = this.sentinel;
+	if (tmp === item){
+		tmp.next = this.sentinel;
+		this.sentinel.prev = tmp;
+	}
+	while (tmp !== item){
+		tmp = tmp.next;
+	}
+	return this;
+},
+remove: function remove(item){
+	var tmp = this.first();
+	if (tmp.next === item){
+		tmp.next = item.next;
+		item.next.prev = tmp;
+	}
+	while (tmp.next !== item){
+		tmp = tmp.next;
+	}
+	return item.value;
+},
+pop: function pop(){
+	return this.reomve(this.last);
+},
+shift: function shift(){
+	return this.remove(this.first());
+},
+isFirst: function isFirst(item){
+	if (this.first() === item){
+		return true;
+	}
+	return false;
+},
+isLast: function isLast(item){
+	if (this.last() === item){
+		return true;
+	}
+	return false;
+},
+iterator: function iterator(){
+	var itr, that;
+	that = this;
+	itr = this.sentinel;
+	return Iterator.new(
+		function next(){
+			itr = itr.next;
+			return itr.value;
+		},
+		function hasNext(){
+			return itr.next !== that.sentinel;
+		}
+	);
+},
+forEach: function forEach(f){
+	return this.iterator().forEach(f);
+},
+toArray: function toArray(){
+	return this.iterator().toArray();
+},
+iterateFrom: function iterateFrom(item){
+	var that = this;
+	item = item.prev;
+	return Iterator.new(
+		function next(){
+			item = item.next;
+			return item.value;
+		},
+		function hasNext(){
+			return item.next !== that.sentinel;
+		}
+	);
+},
+reverseIterateFrom: function reverseiterateFrom(item){
+	var that = this;
+	item = item.next;
+	return Iterator.new(
+		function next(){
+			item = item.prev;
+			return item.value;
+		},
+		function hasNext(){
+			return item.prev !== that.sentinel;
+		}
+	);
+}
 };
-
-
-
 // DO NOT MODIFY ANYTHING BELOW THIS LINE
 DLList = {
-   new: makeNewList
+new: makeNewList
 };
-
 Object.defineProperty(DLList, "prototype", {
-   value: proto,
-   writable: false
+value: proto,
+writable: false
 });
-
 module.exports = DLList;
